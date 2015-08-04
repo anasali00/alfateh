576c2609e73d4407504b31844e16add60ab4664b
anasali00 <theanasali@gmail.com>
2015-08-04 13:17:29 +0500

    Update customers.py

--- customers/customers.py
+++ customers/customers.py
@@ -6,5 +6,5 @@ class customer_custom(models.Model):
     supplier_ntn = fields.Char(string="Supplier NTN")
     supplier_cnic = fields.Char(string="Supplier CNIC")
     buyer_type = fields.Selection([('registered','Registered'), ('unregistered','Unregistered'),('bulk_unregistered','Bulk-Unregistered'),('retail_customer','Retail Consumers')],"Buyer Type")
-    province = fields.Selection([('punjab','Punjab'), ('sindh','Sindh'),('balochistan','Balochistan'),('kpk','KPK')],"Province")
+    province = fields.Selection([('punjab','Punjab'), ('sindh','Sindh'),('balochistan','Balochistan'),('capital','Capital Territory'),('kpk','KPK')],"Province")
     
